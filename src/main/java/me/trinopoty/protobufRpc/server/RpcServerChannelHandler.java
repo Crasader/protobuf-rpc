bfa585a8a148a15459cf8f6169c8c76ac67f1bdb
Trinopoty Biswas <trinopoty@hotmail.com>
2017-08-18 16:44:58 +0530

    Code cleanup

--- src/main/java/me/trinopoty/protobufRpc/server/RpcServerChannelHandler.java
+++ src/main/java/me/trinopoty/protobufRpc/server/RpcServerChannelHandler.java
@@ -9,7 +9,6 @@ import me.trinopoty.protobufRpc.util.RpcServiceCollector;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.HashMap;
 
 final class RpcServerChannelHandler extends ChannelInboundHandlerAdapter {
