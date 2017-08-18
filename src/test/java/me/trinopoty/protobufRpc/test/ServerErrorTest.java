fa755631e8c671f16303f1ed7fab581932301d04
Trinopoty Biswas <trinopoty@hotmail.com>
2017-08-18 17:56:56 +0530

    Added 'ServerErrorTest'

---
+++ src/test/java/me/trinopoty/protobufRpc/test/ServerErrorTest.java
@@ -0,0 +1,54 @@
+package me.trinopoty.protobufRpc.test;
+
+import me.trinopoty.protobufRpc.annotation.RpcIdentifier;
+import me.trinopoty.protobufRpc.client.ProtobufRpcClient;
+import me.trinopoty.protobufRpc.client.RpcClientChannel;
+import me.trinopoty.protobufRpc.exception.RpcCallServerException;
+import me.trinopoty.protobufRpc.server.ProtobufRpcServer;
+import me.trinopoty.protobufRpc.test.proto.EchoOuterClass;
+import org.junit.Test;
+
+public final class ServerErrorTest {
+
+    @RpcIdentifier(1)
+    public interface EchoService01 {
+
+        @RpcIdentifier(1)
+        EchoOuterClass.Echo echo(EchoOuterClass.Echo request);
+    }
+
+    @RpcIdentifier(2)
+    public interface EchoService02 {
+
+        @RpcIdentifier(1)
+        EchoOuterClass.Echo echo(EchoOuterClass.Echo request);
+    }
+
+    public static final class EchoService01Impl implements EchoService01 {
+
+        @Override
+        public EchoOuterClass.Echo echo(EchoOuterClass.Echo request) {
+            return request;
+        }
+    }
+
+    @Test(expected = RpcCallServerException.class)
+    public void serverTest01() {
+        ProtobufRpcServer.Builder builder = new ProtobufRpcServer.Builder();
+        builder.setLocalPort(6000);
+        ProtobufRpcServer server = builder.build();
+        server.addServiceImplementation(EchoService01.class, EchoService01Impl.class);
+        server.startServer();
+
+        ProtobufRpcClient client = (new ProtobufRpcClient.Builder()).build();
+        RpcClientChannel clientChannel = client.getClientChannel("127.0.0.1", 6000);
+
+        EchoService02 echoService02 = clientChannel.getService(EchoService02.class);
+        echoService02.echo(EchoOuterClass.Echo.newBuilder().setMessage("Hello").build());
+
+        clientChannel.close();
+        client.close();
+
+        server.stopServer();
+    }
+}
